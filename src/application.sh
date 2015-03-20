b50a471e2b68a49ba97c19590d389ee391cd6cdc
Rodolphe Pasquier <rodolphe.pasquier@sfr.fr>
2015-03-20 15:58:14 +0100

    Add var to script

--- src/application.sh
+++ src/application.sh
@@ -1,8 +1,8 @@
 ### application.sh - 2015 - Pasquier Rodolphe ###
+version = "2"
+build_hour = "11H00"
 
-echo "Build : 10:00"
-
-echo "version 1.1"
-
+echo "Build ${build_hour}"
+echo "version ${version}"
 
 echo "Copyright company - 2015"
